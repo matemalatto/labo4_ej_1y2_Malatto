e1e30b3b3336edd412026d5feb39391e329134f9
Mateo <mateomalatto@gmail.com>
2024-08-31 15:42:47 -0300

    ej 1 y 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
-  title = 'primer-proyecto';
+  edadUno: number = 0;  
+  edadDos: number = 0;  
+  suma: number = 0;  
+  promedio: number = 0; 
+
+  calcular() {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  limpiar() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"login", component: LoginComponent},
+    {path:"error", component: ErrorComponent},
+    {path:"bienvenido", component: BienvenidoComponent}
+];
